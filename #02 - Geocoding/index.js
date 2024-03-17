89fc9cd74d05022d2f8c5dcfebf39799344aa2da
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2024-03-16 20:45:51 -0700

    Geocoding edit
    Now the API works, but the input doesn't gets the values.

--- #02 - Geocoding/index.js
+++ #02 - Geocoding/index.js
@@ -1,5 +1,28 @@
+const cityName = document.querySelector('.miInput');
+const button = document.querySelector("button");
+const apiKey = '78a3af19d8f5570f8ee3f138a3c05c4a';
+//const cityName = 'London';
+//const stateCode = '';
+//const countryCode = ''; // Código de país para el Reino Unido
+const limit = 4; // Límite de resultados
+
+const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`;
+
 function miFunc() {
-    alert('Se ha dado clic al botón!');
+  fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al hacer la solicitud');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Hacer algo con los datos obtenidos
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
   }
 
 
