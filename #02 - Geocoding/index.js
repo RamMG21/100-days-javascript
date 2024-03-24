121673be915a3c60b06a62d1308b64074275315a
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2024-03-23 20:11:42 -0700

    update index.js

--- #02 - Geocoding/index.js
+++ #02 - Geocoding/index.js
@@ -4,11 +4,12 @@ const apiKey = '78a3af19d8f5570f8ee3f138a3c05c4a';
 //const cityName = 'London';
 //const stateCode = '';
 //const countryCode = ''; // Código de país para el Reino Unido
-const limit = 4; // Límite de resultados
+const limit = 2; // Límite de resultados
 
 const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`;
 
 function miFunc() {
+  
   fetch(url)
   .then(response => {
     if (!response.ok) {
@@ -23,6 +24,7 @@ function miFunc() {
   .catch(error => {
     console.error('Error:', error);
   });
+  
   }
 
 
